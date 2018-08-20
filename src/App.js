610c10d9f441cd508e42f1d5ae34ba2d9c491dfd
Krzysztof Stefanski <krzysztof.stefanski82@gmail.com>
2018-08-20 14:00:32 +0200

    add router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom'
 
-class App extends Component {
+
+class App extends React.Component {
   render() {
     return (
       <div>
-        
+        <Router>
+          <div>
+            <Link to={'/hello/world'}> World</Link>
+            <Link to={'/hello'}> Hello</Link>
+           <Switch>
+        <Route path={'/'} exact component={Hello} />
+        <Route path={'/hello'} component={Hello} />
+        <Route path={'/hello-world'} component={World} />
+        </Switch>
+        </div>
+        </Router>
+
       </div>
     );
   }
 }
+
+const Hello = function(props){
+  return <p>Hello</p>
+}
+const World = props => (
+  <div>World</div>
+)
 
 export default App;
